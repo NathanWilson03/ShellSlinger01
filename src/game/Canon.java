336cee30c8d635864f8a55ee8aca8f3abfcb7310
natewilson <natewilson869@gmail.com>
2025-03-06 18:07:51 -0500

    Added two canon objects

--- src/game/Canon.java
+++ src/game/Canon.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Canon extends Polygon{
 
 	// two types of canon. Aiming, which shoots the bullet object and is controlled by the user. 
@@ -9,5 +12,32 @@ public class Canon extends Polygon{
 		super(inShape, inPosition, inRotation);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+	
+	public void paint(Graphics brush) {
+		
+		
+		
+		Point[] pArray = super.getPoints();
+		int[] xPoints = new int[pArray.length];
+		int[] yPoints = new int[pArray.length];
+		
+		for(int i = 0; i < pArray.length; i++) {
+			xPoints[i] = (int)pArray[i].x;
+			yPoints[i] = (int)pArray[i].y;
+			
+			
+		}
+		
+		
+		
+		brush.fillPolygon(xPoints, yPoints, pArray.length);
+		
+		
+		
+		
+		
+		
+	}
 }
--- src/game/ShellSlinger.java
+++ src/game/ShellSlinger.java
@@ -14,9 +14,10 @@ class ShellSlinger extends Game {
 	static int counter = 0;
 
   public ShellSlinger() {
-    super("YourGameName!",800,600);
+    super("ShellSlinger",800,600);
     this.setFocusable(true);
 	this.requestFocus();
+	
   }
   
 	public void paint(Graphics brush) {
@@ -26,10 +27,40 @@ class ShellSlinger extends Game {
     	// sample code for printing message for debugging
     	// counter is incremented and this message printed
     	// each time the canvas is repainted
-    	counter++;
-    	brush.setColor(Color.white);
-    	brush.drawString("Counter is " + counter,10,10);
+    	
+    	
+    	
+    	// this is our cannon object shape 
+    	Point p1 = new Point(40,10);
+    	Point p2= new Point(40,40);
+		Point p3 = new Point(10,40);
+		
+		Point p4 = new Point(0,50);
+		Point p5 = new Point(0,0);
+		Point p6 = new Point(10, 10);
+		
+		
+	
+		
+		Point[] canonPoints = {p1,p2,p3,p4,p5,p6};
+		
+		
+    	Canon startCanon = new Canon(canonPoints, new Point(10,550), 133);
+    	Canon aimCanon = new Canon(canonPoints, new Point(765, 550), 60);
+    	
+    	brush.setColor(new Color(34, 177, 76));
+    	startCanon.paint(brush);
+    	
+    	brush.setColor(new Color(160,40,20));
+    	
+    	aimCanon.paint(brush);
+    	
+    	
+   
+    	
   }
+	
+	
   
 	public static void main (String[] args) {
    		ShellSlinger a = new ShellSlinger();
